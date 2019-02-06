5443773c1b36f59fce6a85ab5923cf97e661faf8
ShigeakiS <shigeaki@shigeakinoMacBook-puro.local>
2019-02-06 19:33:19 +0900

    retouch_file

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,12 @@ module MercariF35
   class Application < Rails::Application
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
     config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
