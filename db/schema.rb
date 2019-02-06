cf1ee73e2fa08b28073b1c2a627254717c06a49f
yoshikawaya <yoshikawakosuke@yoshikawakousukenoMacBook-puro.local>
2019-02-06 18:54:58 +0900

    add_category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190204144320) do
+ActiveRecord::Schema.define(version: 20190206050122) do
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",          null: false
+    t.string   "depth"
+    t.integer  "size_pulldown", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "sns_credentials", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "uid"
