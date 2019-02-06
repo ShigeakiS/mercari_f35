0f1573414d435f2263b6067afee21f26a06ee0ab
ShigeakiS <shigeaki@shigeakinoMacBook-puro.local>
2019-02-06 15:19:46 +0900

    add_item_model_and_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190204144320) do
+ActiveRecord::Schema.define(version: 20190206045943) do
+
+  create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",                        null: false
+    t.text     "description",   limit: 65535, null: false
+    t.integer  "price",                       null: false
+    t.integer  "condition",                   null: false
+    t.integer  "shipping_from",               null: false
+    t.integer  "shipping_date",               null: false
+    t.integer  "shipping_fee",                null: false
+    t.integer  "shipping_way",                null: false
+    t.integer  "user_id"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.index ["user_id"], name: "index_items_on_user_id", using: :btree
+  end
 
   create_table "sns_credentials", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "uid"
@@ -34,5 +49,6 @@ ActiveRecord::Schema.define(version: 20190204144320) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "items", "users"
   add_foreign_key "sns_credentials", "users"
 end
